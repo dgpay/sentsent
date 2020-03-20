1085e5e70cc4ba2ed566ca9a2ec97aa58ad86dbc
dpay <deviap485@gmail.com>
2020-03-20 22:13:45 +0700

    rubah preposessing edit

---
+++ dataset prepos edit/readme.txt
@@ -0,0 +1,25 @@
+data train.csv itu data tesst
+pretest.csv itu data train
+
+File progresedit.py
+1) install nltk
+   untuk menggunakan sastrawi
+2) instal re untuk membantu preposcesising
+
+File duplicate.py
+1) install itertools
+
+
+
+Tata cara penggunaan kodingan
+
+untuk preposcesing kedua file data set dimasukan ke program ini dulu
+1) duplicate
+2) progresedit.py
+
+
+setelah bersih dimasukan ke
+1) exe.py
+
+
+
--- progresedit.py
+++ progresedit.py
@@ -11,32 +11,44 @@ from Sastrawi.Stemmer.StemmerFactory import StemmerFactory
 
 
 dataset = pd.read_csv(
-        'data train.csv',
+        'file2.csv',
         delimiter=';', 
         )
 
 
 arr=dataset.to_numpy()
 kalimat=arr[:,[0]]
+skor=arr[:,[1]]
 
 factory = StemmerFactory()
 stemmer = factory.create_stemmer()
 akhir=[]
+akhirnomor=[]
 i=0
-regex = re.compile('[^a-zA-Z]')
+#regex = re.compile('[^a-zA-Z]')
 for i in range(0,len(kalimat)):
     #print(kalimat[i][0])
     kal=kalimat[i][0]
+    l=skor[i][0]
+    #print(kal)
+    #print(l)
     #print(kal)
     #print("EKSEKUSI BOSSQUE")
     hasil = kal.lower()
 
     hasil = re.sub(r'#(\S+)', '', hasil)
     hasil = re.sub(r'((www\.[\S]+)|(https?://[\S]+)|(pic\.[\S]+))', '', hasil)
+    hasil = re.sub(r'(https[\S]+)|(pic\.[\S]+)', '', hasil)
+    hasil = re.sub(r'(https:/[\S]+)|(pic\.[\S]+)', '', hasil)
+    hasil = re.sub(r'\brt\b', '',hasil)
+    hasil = re.sub(r'@[\S]+', '',hasil)
+    hasil = re.sub(r'#(\S+)', r' \1 ',hasil)
     hasil = re.sub(r'@[\S]+', '', hasil)
     hasil = re.sub(r'\.{2,}', '', hasil)
     hasil = re.sub("[\b\.\b]{3}", " ", hasil)
     hasil = re.sub(r'\s+', ' ', hasil)
+    hasil = re.sub(r'\n+', ' ', hasil)
+    hasil = re.sub(r"[\s\.\n]", " " , hasil)
     hasil = re.sub(r"\d+", "", hasil)
     hasil = hasil.strip()
     hasil = stemmer.stem(hasil)
@@ -46,8 +58,9 @@ for i in range(0,len(kalimat)):
      
     #print('ini ke - ',i)
     akhir.append(hasil)
+    akhirnomor.append(l)
 
 
-dict = {'text': akhir}  
+dict = {'text': akhir,'score':l}  
 df = pd.DataFrame(dict) 
-df.to_csv('xxx.csv') 
+df.to_csv('file2-next.csv') 
